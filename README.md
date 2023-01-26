af3bc3372ffa26fc011e35d66c63650afef2a6a4
Meloddy-Jimenez <122809763+Meloddy-Jimenez@users.noreply.github.com>
2023-01-26 19:50:40 +0000

    Acceso a las variables por posición y nombre

--- README.md
+++ README.md
@@ -7,4 +7,5 @@
 |1.|Programa0.py|Hola mundo en python|
 |2.|programa1.py|programa para ver comentarios multilinia, comentarios de una linea y concatenar cadenas|
 |3.|programa2.py|concatenacion usando formas|
-|4.|programa2.py|Operaciones aritmeticas 
+|4.|programa3.py|Operaciones aritmeticas 
+|5.|programa4.py|Acceso a las variables por posicion y nombre|
---
+++ semana2/programa4.py
@@ -0,0 +1,13 @@
+"""Programa2
+   Nombre: Meloddy Sofia R,J
+   Fecha: 26/01/2023
+   Descripción:Acceso a las variables por posicion y nombre
+
+"""
+numero1 = 10
+numero2 = 5
+print ("{n1} + {n2} = {suma}" .format (n1=numero1 , n2=numero2 , suma=numero1 + numero2))
+
+print ("{numero1} + {numero2} = {suma}" .format (numero1=numero1 , numero2=numero2 , suma=numero1 + numero2))
+
+print ("{numero1} + {numero2} = " .format (numero1 , numero2 , suma=numero1 + numero2))

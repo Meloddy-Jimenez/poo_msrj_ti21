fe2fb73fb28f259231af1d49e9a9376379ec74cb
Meloddy-Jimenez <122809763+Meloddy-Jimenez@users.noreply.github.com>
2023-01-26 19:28:39 +0000

    Realizamos operaciones aritméticas

--- README.md
+++ README.md
@@ -7,3 +7,4 @@
 |1.|Programa0.py|Hola mundo en python|
 |2.|programa1.py|programa para ver comentarios multilinia, comentarios de una linea y concatenar cadenas|
 |3.|programa2.py|concatenacion usando formas|
+|4.|programa2.py|Operaciones aritmeticas 
---
+++ semana2/programa3.py
@@ -0,0 +1,17 @@
+"""Programa2
+   Nombre: Meloddy Sofia R,J
+   Fecha: 24/01/2023
+   Descripción: Operaciones de aritmetica 
+
+"""
+numero1 =10
+numero2 =5
+
+print ("el resultado de sumar {} + {} =" .format (numero1 , numero2))
+print (numero1 + numero2)
+print ("el resultado de multiplicar {} * {} =" .format (numero1 , numero2))
+print (numero1 * numero2)
+print ("el resultado de dividir {} / {} =" .format (numero1 , numero2))
+print (numero1 / numero2)
+print ("el resultado de restar {} - {} =" .format (numero1 , numero2))
+print (numero1 - numero2)

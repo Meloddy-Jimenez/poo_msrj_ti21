c721ca4a68c32713e36553ba421f7e90a5034d22
Meloddy-Jimenez <122809763+Meloddy-Jimenez@users.noreply.github.com>
2023-01-30 16:57:19 +0000

    creamos la carpeta semana3 y elaboramos operaciones básicas utilizando int

--- README.md
+++ README.md
@@ -9,3 +9,4 @@
 |3.|programa2.py|concatenacion usando formas|
 |4.|programa3.py|Operaciones aritmeticas 
 |5.|programa4.py|Acceso a las variables por posicion y nombre|
+|6.|programa5.py|operaciones basicas utilizando int|
---
+++ semana3/programa5.py
@@ -0,0 +1,18 @@
+"""Programa5
+   Nombre: Meloddy Sofia R,J
+   Fecha: 30/01/2023
+   Descripción: operaciones basicas utilizando int
+
+"""
+numero1 = input("Numero1 :")
+numero2 = input("numero2 :")
+suma = int(numero1) + int(numero2)
+print (suma)
+resta = int(numero1) - int(numero2)
+print (resta)
+multiplicacion = int(numero1) * int(numero2)
+print (multiplicacion)
+division = int(numero1) / int(numero2)
+print (division)
+potencia = int(numero1) ** int(numero2)
+print (potencia)

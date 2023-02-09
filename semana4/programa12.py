1ccdf006dbab53aad9f606b551daac730265a0a0
Meloddy-Jimenez <122809763+Meloddy-Jimenez@users.noreply.github.com>
2023-02-09 20:14:43 +0000

    Se crearan 8 pruebas utilizando "mayor" para probar que los codigos sean funcionales codigo2

--- semana4/programa12.py
+++ semana4/programa12.py
@@ -4,4 +4,20 @@
    Descripción: Se crearan 8 pruebas utilizando "mayor" para probar que los codigos sean funcionales codigo2
 
 """
-def mayor(numero1:int,numero2:int)- int|None:
+def mayor(numero1:int,numero2:int)-int|None: 
+    result = None
+    if numero1 > numero2:
+        result = numero1
+    elif numero2 > numero1:
+         result = numero2
+    return result
+
+print(mayor(2,1))
+print(mayor(1,2))
+print(mayor(2,2))
+print(mayor(2,-1))
+print(mayor(-1,2))
+print(mayor(-1,-1))
+print(mayor(-2,-1))
+print(mayor(-1,-2))
+    
